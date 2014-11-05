0f0803564cf3ac8e24c43daeb97f4a85056283c9
Contra <contra@maricopa.edu>
2014-11-05 09:49:43 -0700

    initial swipe right and left with rotation

--- samples/sandbox/src/index.js
+++ samples/sandbox/src/index.js
@@ -11,8 +11,31 @@ var App = React.createClass({
   getInitialState: function(){
     return {};
   },
+  swipedRight: function(){
+    console.log('swiped right');
+  },
+  swipedLeft: function(){
+    console.log('swiped left');
+  },
   render: function(){
-    return null;
+    var child = React.DOM.div({
+      style: {
+        height: 250,
+        width: 250,
+        backgroundColor: 'red'
+      }
+    });
+
+    var swipeable = Swipeable({
+      onSwipeRight: this.swipedRight,
+      onSwipeLeft: this.swipedLeft
+    }, child);
+
+    var container = React.DOM.div({
+      className: 'demo-container'
+    }, swipeable);
+
+    return container;
   }
 });
 
--- src/index.js
+++ src/index.js
@@ -6,24 +6,82 @@ var PropTypes = require('react/lib/ReactPropTypes');
 
 var Draggable = require('react-draggable');
 
+var rotate = function(deg) {
+  var v = 'rotate('+deg+'deg)';
+  return {
+    transform: v,
+    webkitTransform: v,
+    mozTransform: v,
+    oTransform: v,
+    msTransform: v
+  };
+};
+
+// TODO: curb rotation
+// TODO: rotate back if stopped and didnt swipe right or left
+
 var Swipeable = ReactCompositeComponent.createClass({
   displayName: 'Swipeable',
   propTypes: {
+    onDragStart: PropTypes.func,
+    onDragStop: PropTypes.func,
     onDrag: PropTypes.func,
     onSwipeRight: PropTypes.func,
-    onSwipeLeft: PropTypes.func
+    onSwipeLeft: PropTypes.func,
+    zIndex: PropTypes.number,
+    rotationAngle: PropTypes.number
   },
 
-  getDefaultProps: function() {
-    return {};
+  getDefaultProps: function(){
+    return {
+      rotationAngle: 10
+    };
   },
 
-  getInitialState: function() {
-    return {};
+  getInitialState: function(){
+    return {
+      rotation: 0
+    };
+  },
+
+  componentDidMount: function(){
+    var el = this.getDOMNode();
+    this.setState({
+      breakpoint: el.clientWidth / 2
+    });
+  },
+
+  handleDrag: function(event, ui){
+    if (ui.position.left >= this.state.breakpoint) {
+      this.props.onSwipeRight();
+    } else if (ui.position.left <= -this.state.breakpoint) {
+      this.props.onSwipeLeft();
+    }
+    this.setState({
+      rotation: ui.position.left / this.props.rotationAngle
+    });
+
+    if (this.props.onDrag) {
+      this.props.onDrag(event, ui);
+    }
+
+    console.log(ui.position.left);
   },
 
   render: function(){
-    return null;
+    var draggable = Draggable({
+      axis: 'x',
+      onStart: this.props.onDragStart,
+      onStop: this.props.onDragStop,
+      onDrag: this.handleDrag,
+      zIndex: this.props.zIndex,
+      ranges: {
+        x: [-this.state.breakpoint, this.state.breakpoint]
+      },
+      style: rotate(this.state.rotation)
+    }, this.props.children);
+
+    return draggable;
   }
 });
 
